90b0825259cc78c3bf170087aa3f1f069754c7de
vundyalaavinash <vundyala.avinash@gmail.com>
2013-08-18 13:21:01 +0530

    Before sending to mubarakh

--- web/CusScripts/ENB.js
+++ web/CusScripts/ENB.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 	$( "#tabs" ).tabs();
+	
+	$("#atab1").click(function(){
+			$("#atab1").addClass("present");
+			$("#atab2").removeClass("present");
+			$("#atab3").removeClass("present");
+			$("#atab4").removeClass("present");
+	});
+	$("#atab2").click(function(){
+			$("#atab2").addClass("present");
+			$("#atab1").removeClass("present");
+			$("#atab3").removeClass("present");
+			$("#atab4").removeClass("present");
+	});
+	$("#atab3").click(function(){
+			$("#atab3").addClass("present");
+			$("#atab2").removeClass("present");
+			$("#atab1").removeClass("present");
+			$("#atab4").removeClass("present");
+	});
+	$("#atab4").click(function(){
+			$("#atab4").addClass("present");
+			$("#atab2").removeClass("present");
+			$("#atab3").removeClass("present");
+			$("#atab1").removeClass("present");
+	});
 });
